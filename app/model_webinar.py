c0939e619e80a84c3d5d3058901c4d5fb29cde90
vedsu <90701239+vedsu@users.noreply.github.com>
2024-07-08 12:12:59 +0530

    Update model_webinar.py

--- app/model_webinar.py
+++ app/model_webinar.py
@@ -5,6 +5,55 @@ from app import mongo
 class Webinar():
 
     @staticmethod
+    def data_webinar(w_id, website):
+        
+        webinar_info = None
+        try: 
+            
+            webinar_data = list(mongo.db.webinar_data.find({"$and":[{"id":w_id}, {"website":website}]}))
+            webinar = webinar_data[0]
+               
+            webinar_data_dict ={
+            
+                    "id":webinar ["id"],
+
+                    "topic":webinar ["topic"],
+                    "industry":webinar ["industry"],
+                    "speaker":webinar ["speaker"],
+                    "date":webinar ["date_time"],
+                    "time":webinar ["time"],
+                    "timeZone":webinar["timeZone"],
+                    "duration":webinar["duration"],
+                    "category":webinar["category"],
+                    
+                    "sessionLive":webinar ["sessionLive"],
+                    "priceLive":webinar ["priceLive"],
+                    "urlLive":webinar ["urlLive"],
+                    
+                    "sessionRecording":webinar ["sessionRecording"],
+                    "priceRecording":webinar ["priceRecording"],
+                    "urlRecording":webinar ["urlRecording"],
+
+                    "sessionDigitalDownload":webinar ["sessionDigitalDownload"],
+                    "priceDigitalDownload":webinar ["priceDigitalDownload"],
+                    "urlDigitalDownload":webinar ["urlDigitalDownload"],
+                    
+                    "sessionTranscript":webinar ["sessionTranscript"],
+                    "priceTranscript":webinar ["priceTranscript"],
+                    "urlTranscript":webinar ["urlTranscript"],
+
+                    "status":webinar ["status"],
+                    "webinar_url": webinar ["webinar_url"],
+                    "description":webinar ["description"],
+
+                    }
+            webinar_info = webinar_data_dict
+        except Exception as e:
+            webinar_info = None
+        
+        return webinar_info
+    
+    @staticmethod
     def view_webinar():
         webinar_list = []
         try:
@@ -49,3 +98,5 @@ class Webinar():
         except Exception as e:
             webinar_list = []
         return webinar_list
+
+
