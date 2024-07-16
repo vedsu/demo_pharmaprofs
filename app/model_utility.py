766ced2c49a470c7807a3635ab82d33abe223582
vedsu <90701239+vedsu@users.noreply.github.com>
2024-07-16 10:47:20 +0530

    Update model_utility.py

--- app/model_utility.py
+++ app/model_utility.py
@@ -1,1 +1,25 @@
+# Utilities
+from app import mongo
+from datetime import datetime, timedelta
 
+class Utility():
+  @staticmethod
+    def subscribe(subscriber):
+       current_datetime = datetime.now()
+       try:
+            mongo.db.subscriber_list.insert_one({"email":subscriber, "type":"subscriber", "date":current_datetime})
+            return ({"success": True, "message": "subscribed successfully"}),201
+       except Exception as e:
+            return ({"success": False, "message": str(e)}), 403
+
+  @staticmethod
+    def unsubscribe(unsubscriber):
+       current_datetime = datetime.now()
+       try:
+            mongo.db.subscriber_list.insert_one({"email":unsubscriber, "type":"unsubscriber", "date":current_datetime})
+            return ({"success": True, "message": "unsubscribed successfully"}),201
+       except Exception as e:
+            return ({"success": False, "message": str(e)}), 403
+         
+
+      
